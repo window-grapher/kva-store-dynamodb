4d9e0ee768970d8249dd57a0ef0a93eed8aa6365
takoyaki-3 <mail@takoyaki3.com>
2024-01-31 20:36:56 +0900

    Initial commit

---
+++ test/kva-store-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KvaStoreDynamodb from '../lib/kva-store-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kva-store-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KvaStoreDynamodb.KvaStoreDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
