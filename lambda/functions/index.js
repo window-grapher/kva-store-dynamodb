842fd73f0b2575a300003df8f7c3b31b9edb1904
takoyaki-3 <mail@takoyaki3.com>
2024-01-31 22:30:50 +0900

    GETも関数を分離

--- lambda/functions/index.js
+++ lambda/functions/index.js
@@ -5,56 +5,62 @@ exports.handler = async (event) => {
     const httpMethod = event.requestContext.http.method;
     try {
         // POSTリクエストの処理
-        if (httpMethod === 'POST') {
-            return await handlePostRequest(event);
+        switch(httpMethod){
+          case 'GET':
+            return await handleGetRequest(event)
+          case 'POST':
+            return await handlePostRequest(event)
         }
-        if (!event.queryStringParameters || !event.queryStringParameters.key) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ message: "Key parameter is required" }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            };
-        }
-
-        const keyParam = event.queryStringParameters.key;
-        let limitParam = parseInt(event.queryStringParameters.limit);
-
-        if (isNaN(limitParam) || limitParam < 1) {
-            limitParam = undefined; // またはデフォルト値を設定
-        }
-
-        const params = {
-            TableName: 'keyValueArrayStoreTable', // DynamoDBテーブル名
-            KeyConditionExpression: '#key = :keyValue', // '#key'を使用
-            ExpressionAttributeNames: {
-                '#key': 'key' // '#key'は実際の属性名'key'を指す
-            },
-            ExpressionAttributeValues: {
-                ':keyValue': keyParam
-            },
-            ScanIndexForward: false // 新しい順にソート
-        };
-
-        if (limitParam) {
-            params.Limit = limitParam;
-        }
-
-        const data = await dynamoDB.query(params).promise();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data.Items),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
     } catch (error) {
       console.error(error);
       return errorResponse(error.message);
   }
 };
+
+async function handleGetRequest(event) {
+  if (!event.queryStringParameters || !event.queryStringParameters.key) {
+    return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Key parameter is required" }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+  }
+
+  const keyParam = event.queryStringParameters.key;
+  let limitParam = parseInt(event.queryStringParameters.limit);
+
+  if (isNaN(limitParam) || limitParam < 1) {
+      limitParam = undefined; // またはデフォルト値を設定
+  }
+
+  const params = {
+      TableName: 'keyValueArrayStoreTable', // DynamoDBテーブル名
+      KeyConditionExpression: '#key = :keyValue', // '#key'を使用
+      ExpressionAttributeNames: {
+          '#key': 'key' // '#key'は実際の属性名'key'を指す
+      },
+      ExpressionAttributeValues: {
+          ':keyValue': keyParam
+      },
+      ScanIndexForward: false // 新しい順にソート
+  };
+
+  if (limitParam) {
+      params.Limit = limitParam;
+  }
+
+  const data = await dynamoDB.query(params).promise();
+
+  return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items),
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  };
+}
 
 async function handlePostRequest(event) {
   const body = JSON.parse(event.body);
