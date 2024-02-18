4c1fd4f4cb734f63c1c7415d20362a04f2ebcf87
takoyaki-3 <mail@takoyaki3.com>
2024-02-18 18:09:39 +0900

    add test case

--- test/kva-store-dynamodb.test.js
+++ test/kva-store-dynamodb.test.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 require('dotenv').config();
 
+const apiUrl = process.env.API_URL;
+const secretToken = process.env.TEST_SECRET_TOKEN;
+const adminSecretToken = process.env.SECRET_TOKEN;
+
 describe('Get, Add, Delete by anonymous', () => {
-  const apiUrl = process.env.API_URL;
-  const secretToken = process.env.SECRET_TOKEN;
 
   beforeEach(async () => {
     const response = await axios.get(`${apiUrl}?key=testKey`);
@@ -15,9 +17,11 @@ describe('Get, Add, Delete by anonymous', () => {
     items.forEach(async (item) => {
       const resp = await axios.delete(apiUrl, {
         data: { key: item.key, created: item.created },
-        headers: { SecretToken: `${secretToken}` }
+        headers: { SecretToken: `${adminSecretToken}` }
       });
     });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
   });
 
   it('An empty array should be returned when a GET request is made in an empty state.', async () => {
@@ -32,6 +36,8 @@ describe('Get, Add, Delete by anonymous', () => {
     // Add data
     await axios.post(apiUrl, { key: 'testKey', data: 'testData' });
 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     // Get added data
     const response = await axios.get(`${apiUrl}?key=testKey&limit=10`);
     expect(response.status).toBe(200);
@@ -42,13 +48,15 @@ describe('Get, Add, Delete by anonymous', () => {
     // Add data
     await axios.post(apiUrl, { key: 'testKey', data: 'testData' });
 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     // Delete the data
     const response = await axios.get(`${apiUrl}?key=testKey`);
     const items = response.data;
     items.forEach(async (item) => {
       const resp = await axios.delete(apiUrl, {
         data: { key: item.key, created: item.created },
-        headers: { SecretToken: `${secretToken}` }
+        headers: { SecretToken: `${adminSecretToken}` }
       });
     });
 
@@ -63,9 +71,6 @@ describe('Get, Add, Delete by anonymous', () => {
 });
 
 describe('Get, Add, Delete by authenticated user', () => {
-  const apiUrl = process.env.API_URL;
-  const secretToken = process.env.TEST_SECRET_TOKEN;
-
   beforeEach(async () => {
     const response = await axios.get(`${apiUrl}?key=authTestKey`, {
       headers: { SecretToken: `${secretToken}` }
@@ -80,6 +85,7 @@ describe('Get, Add, Delete by authenticated user', () => {
         headers: { SecretToken: `${secretToken}` }
       });
     }
+    await new Promise((resolve) => setTimeout(resolve, 1000));
   });
 
   it('An empty array should be returned when a GET request is made by an authenticated user in an empty state.', async () => {
@@ -96,6 +102,8 @@ describe('Get, Add, Delete by authenticated user', () => {
       headers: { SecretToken: `${secretToken}` }
     });
 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const response = await axios.get(`${apiUrl}?key=authTestKey`, {
       headers: { SecretToken: `${secretToken}` }
     });
@@ -111,6 +119,8 @@ describe('Get, Add, Delete by authenticated user', () => {
       headers: { SecretToken: `${secretToken}` }
     });
 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const response = await axios.get(`${apiUrl}?key=authTestKey`, {
       headers: { SecretToken: `${secretToken}` }
     });
@@ -122,11 +132,126 @@ describe('Get, Add, Delete by authenticated user', () => {
       });
     }
 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const afterResponse = await axios.get(`${apiUrl}?key=authTestKey&limit=10`, {
       headers: { SecretToken: `${secretToken}` }
     });
     expect(afterResponse.status).toBe(200);
     expect(Array.isArray(afterResponse.data)).toBe(true);
     expect(afterResponse.data.length).toBe(0);
+  });
+});
+
+describe('Readable field behavior', () => {
+  const testKeyForReadable = 'testKeyReadable';
+  const testDataForReadable = 'testDataReadable';
+
+  it('Data added by an anonymous user should not be accessible by another user when readable is set to private', async () => {
+    // Add data by an anonymous user
+    await axios.post(apiUrl, { key: testKeyForReadable, data: testDataForReadable, readable: 'private' });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // authenticated user tries to get data
+    const response = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${secretToken}` }
+    });
+    expect(response.data.length).toBe(0);
+
+    // Get data as an admin user
+    const adminResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${adminSecretToken}` }
+    });
+    expect(adminResponse.data).toEqual(expect.arrayContaining([expect.objectContaining({ data: testDataForReadable })]));
+  
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Delete data by an anonymous user
+    let cleanupResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`);
+    let items = cleanupResponse.data;
+    // for (const item of items) {
+    //   await axios.delete(apiUrl, {
+    //     data: { key: item.key, created: item.created }
+    //   });
+    // }
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Get data as an admin user
+    let authenticatedGetResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${adminSecretToken}` }
+    });
+    expect(authenticatedGetResponse.data.length).toBe(1);
+
+    // Delete data by an authenticated user
+    // for (const item of items) {
+    //   await axios.delete(apiUrl, {
+    //     data: { key: item.key, created: item.created },
+    //     headers: { SecretToken: `${secretToken}` }
+    //   });
+    // }
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Get data as an admin user
+    let adminGetResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${adminSecretToken}` }
+    });
+    expect(adminGetResponse.data.length).toBe(1);
+    
+    // Delete data by an admin user
+    for (const item of items) {
+      await axios.delete(apiUrl, {
+        data: { key: item.key, created: item.created },
+        headers: { SecretToken: `${adminSecretToken}` }
+      });
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Get data as an admin user
+    adminGetResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${adminSecretToken}` }
+    });
+    expect(adminGetResponse.data.length).toBe(0);
+  });
+
+  it('Data added by an authenticated user should be accessible only when readable matches the condition', async () => {
+    // Add data as an authenticated user
+    await axios.post(apiUrl, { key: testKeyForReadable, data: testDataForReadable, readable: 'authenticated' }, {
+      headers: { SecretToken: `${secretToken}` }
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Anonymous user tries to get data
+    const anonResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`);
+    expect(anonResponse.data.length).toBe(0);
+
+    // Get data as an authenticated user
+    const authResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${secretToken}` }
+    });
+    expect(authResponse.data).toEqual(expect.arrayContaining([expect.objectContaining({ data: testDataForReadable })]));
+
+    // Get data as an admin user
+    const adminResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${adminSecretToken}` }
+    });
+    expect(adminResponse.data).toEqual(expect.arrayContaining([expect.objectContaining({ data: testDataForReadable })]));
+  });
+  
+  // Clear data after each test
+  afterEach(async () => {
+    // Delete data by an admin user
+    const cleanupResponse = await axios.get(`${apiUrl}?key=${testKeyForReadable}`, {
+      headers: { SecretToken: `${adminSecretToken}` }
+    });
+    const items = cleanupResponse.data;
+    for (const item of items) {
+      await axios.delete(apiUrl, {
+        data: { key: item.key, created: item.created },
+        headers: { SecretToken: `${adminSecretToken}` }
+      });
+    }
   });
 });
