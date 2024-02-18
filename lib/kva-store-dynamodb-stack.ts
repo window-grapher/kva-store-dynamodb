0c3b6bacfc7c5678c336bf17c9da80670ae3519c
takoyaki-3 <mail@takoyaki3.com>
2024-02-18 10:51:32 +0900

    add environment name to resorce.

--- lib/kva-store-dynamodb-stack.ts
+++ lib/kva-store-dynamodb-stack.ts
@@ -1,18 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'; // DynamoDBのインポート
-import * as lambda from 'aws-cdk-lib/aws-lambda';    // Lambdaのインポート
-import * as s3 from 'aws-cdk-lib/aws-s3';           // S3のインポート
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+interface KvaStoreDynamodbStackProps extends cdk.StackProps {
+  envName: string; // 環境名を示す新しいプロパティ
+}
 
 export class KvaStoreDynamodbStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: KvaStoreDynamodbStackProps) {
     super(scope, id, props);
+
+    // リソース名に環境名を含める
+    const tableName = `keyValueArrayStoreTable-${props.envName}`;
+    const functionName = `keyValueArrayStoreHandler-${props.envName}`;
+    const bucketName = `keyvaluearraystorebucket-${props.envName.toLowerCase()}`; // S3バケット名は小文字のみ
 
     // DynamoDBテーブルの定義
     const table = new dynamodb.Table(this, 'keyValueArrayStoreTable', {
       partitionKey: { name: 'key', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'created', type: dynamodb.AttributeType.STRING },
-      tableName: 'keyValueArrayStoreTable',
+      tableName: tableName,
       // その他の設定...
     });
     // ローカルセカンダリインデックスの追加
@@ -30,6 +39,7 @@ export class KvaStoreDynamodbStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset('lambda/functions'),
       handler: 'index.handler',
+      functionName: functionName, // 関数名に環境名を含める
       // その他の設定...
     });
     // 関数URLの有効化
@@ -43,8 +53,12 @@ export class KvaStoreDynamodbStack extends cdk.Stack {
 
     // S3バケットの定義
     const bucket = new s3.Bucket(this, 'keyValueArrayStoreBucket', {
+      bucketName: bucketName, // バケット名に環境名を含める
       // バケット設定...
     });
+
+    // DynamoDBテーブルへの権限付与
+    table.grantReadWriteData(myFunction);
 
     // S3バケットへの権限付与
     bucket.grantReadWrite(myFunction);
