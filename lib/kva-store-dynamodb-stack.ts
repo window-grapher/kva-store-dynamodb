67ea5e0ac74481b3e09001b32971db351b0361d7
takoyaki-3 <mail@takoyaki3.com>
2024-04-28 22:04:46 +0900

    Add API Gateway integration for keyValueArrayStore resource

--- lib/kva-store-dynamodb-stack.ts
+++ lib/kva-store-dynamodb-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 interface KvaStoreDynamodbStackProps extends cdk.StackProps {
   envName: string; // e.g. 'staging', 'production'
@@ -19,6 +20,12 @@ export class KvaStoreDynamodbStack extends cdk.Stack {
     const functionName = `keyValueArrayStoreHandler-${props.envName}`;
     const bucketName = `keyvaluearraystorebucket-${props.envName.toLowerCase()}`; // S3 bucket name is case-insensitive
 
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'keyValueArrayStoreBucket', {
+      bucketName: bucketName,
+      // Bucket settings...
+    });
+    
     // DynamoDB table definition
     const table = new dynamodb.Table(this, 'keyValueArrayStoreTable', {
       partitionKey: { name: 'key', type: dynamodb.AttributeType.STRING },
@@ -55,10 +62,17 @@ export class KvaStoreDynamodbStack extends cdk.Stack {
       // function URL settings...
     });
 
-    const functions = [myFunction];
+    // Set up API Gateway
+    const api = new apigateway.RestApi(this, 'keyValueArrayStoreApi', {
+      restApiName: `keyValueArrayStoreApi-${props.envName}`,
+    });
+
+    // Add a resource and method to the API Gateway
+    const resource = api.root.addResource('keyValueArrayStore');
+    resource.addMethod('GET', new apigateway.LambdaIntegration(myFunction));
 
     // Staging test data initialization for DynamoDB
-    if (props.envName === 'staging') {      
+    if (props.envName === 'staging') {
       const initFunction = new lambda.Function(this, 'keyValueArrayStoreTestDataInitHandler', {
         runtime: lambda.Runtime.NODEJS_20_X,
         code: lambda.Code.fromAsset('lambda/functions'),
@@ -70,22 +84,15 @@ export class KvaStoreDynamodbStack extends cdk.Stack {
           AUTHENTICATED_USER_SECRET_TOKEN: props.authenticatedUserSecretToken,
         }
       });
-      functions.push(initFunction);
+
+      // Add permissions
+      table.grantReadWriteData(initFunction);
+      bucket.grantReadWrite(initFunction);
     }
 
-    // Create an S3 bucket
-    const bucket = new s3.Bucket(this, 'keyValueArrayStoreBucket', {
-      bucketName: bucketName,
-      // Bucket settings...
-    });
     
     // Add permissions
-    for (const func of functions) {
-      // DynamoDB table permission
-      table.grantReadWriteData(func);
-
-      // S3 bucket permission
-      bucket.grantReadWrite(func);
-    }
+    table.grantReadWriteData(myFunction);
+    bucket.grantReadWrite(myFunction);
   }
 }
